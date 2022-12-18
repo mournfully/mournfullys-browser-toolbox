73b72954cf3cdf1389bf89fe6d5c79a3f289429c
mournfully <minuramolligoda.dev@gmail.com>
2022-12-17 22:31:11 -0500

    deleted checkboxes from ui.ts

--- src/open-multiple-urls/ui.ts
+++ src/open-multiple-urls/ui.ts
@@ -1,9 +1,5 @@
 export interface UIDef {
   txtArea: HTMLTextAreaElement;
-  lazyLoadCheckbox: HTMLInputElement;
-  randomCheckbox: HTMLInputElement;
-  reverseCheckbox: HTMLInputElement;
-  preserveCheckbox: HTMLInputElement;
   openButton: HTMLInputElement;
   extractButton: HTMLInputElement;
   tabCount: HTMLSpanElement;
@@ -14,10 +10,6 @@ export interface UIDef {
 export function getUIDef(): UIDef {
   return {
     txtArea: document.getElementById('urls') as HTMLTextAreaElement,
-    lazyLoadCheckbox: document.getElementById('lazyLoad') as HTMLInputElement,
-    randomCheckbox: document.getElementById('random') as HTMLInputElement,
-    reverseCheckbox: document.getElementById('reverse') as HTMLInputElement,
-    preserveCheckbox: document.getElementById('preserve') as HTMLInputElement,
     openButton: document.getElementById('open') as HTMLInputElement,
     extractButton: document.getElementById('extract') as HTMLInputElement,
     tabCount: document.getElementById('tabcount') as HTMLSpanElement,
