928cc74601e618ab5a00c5c6fb63a1c9c09da261
mournfully <minuramolligoda.dev@gmail.com>
2023-01-02 22:44:28 -0500

    store state and copy tab urls/titles

--- src/copy-tabs/copy.ts
+++ src/copy-tabs/copy.ts
@@ -1,58 +1,112 @@
-const Lookup = {
-    DataTypes: [
-        { dataFormat:"md", description:"Markdown" },
-        { dataFormat:"title", description:"Title Only" },
-        { dataFormat:"url", description:"URL Only" },
-	]
-};
+import browser from 'webextension-polyfill';
+import ClipboardJS from 'clipboard';
 
-function isImage(url){
-	return (url.indexOf('.jpg') > -1 || url.indexOf('.png') > -1);
-}
-
-// dataFormat - md / title / url
-// dataAction - current / all / all to the right
-function doCopy(dataFormat, dataAction){
-
-    let options = {currentWindow:true};
-	if (dataAction == 'single'){
+// see: https://stackoverflow.com/questions/63526969/how-do-you-return-multiple-values-from-a-typescript-function
+async function setupOptions(dataFormat, dataAction) {
+	//! console.log("func setupOptions: input: ", dataAction);
+	// Query only tabs that are in the current window aka the window that contains the code that is currently executing.
+	let options = { currentWindow: true };
+		
+	// If true, query only the active tabs in their windows.
+	if (dataAction == 'single') {
+		//! console.log("single action");
 		options['active'] = true;
 	}
 
-    let format = 'text/plain';
-    let output = '';
+	let tabStart = 0;
+	if (dataAction == 'rightward') {
+		//! console.log("rightwards action");
+		const currentTab = await getCurrentTab();
+		//! console.log("setupOptions: currentTab:", currentTab);
+		// see: https://developer.chrome.com/docs/extensions/reference/tabs/#type-Tab
+		tabStart = currentTab.index + 1 // all tabs to it's right, but not itself
+		//! +1 means that it'll count all tabs to the right of it, but not itself
+		//! this was done to mimic 'close all tabs to the right' behaviour for consistency
+		//! console.log("setupOptions: tabStart:", tabStart);
+	}
 
-    chrome.tabs.query(options, (tabs)=>{
-        for (let i=0; i < tabs.length; i++){
+	// see: https://www.w3schools.com/js/tryit.asp?filename=tryjs_object_object
+	formatInput(dataFormat, options, tabStart);
+}
+
+/**
+ * Copy urls/titles from tabs in current window
+ * @param dataFormat - type of data format to copy e.g. "md", "title", or "url"
+ * @param dataAction - action when it copies: "current", "all", or "all to the right"
+ */
+function formatInput(dataFormat, options, tabStart) {
+	//! console.log("function formatInput acknowledges:", dataFormat, ",", options, "and", tabStart);
+	let output = '';
+	chrome.tabs.query(options, (tabs) => {
+		for (let i = tabStart; i < tabs.length; i++) {
+			//! console.log("for loop acknowledges")
+
 			let buffer = 'ERROR - buffer was never overwritten by tabTitle or tabUrl';
 			let tabTitle = tabs[i].title;
 			let tabUrl = tabs[i].url;
 
-			// remove leading "notification counts" that websites have started to add, see: https://www.youtube.com/watch?v=bV0QNuhN9fU&t=64s
-			let matches = tabTitle.match(/^(\([0-9]+\) )?(.*)$/);			
+			// remove leading "notification counts" that websites have started to add
+			// see: https://www.youtube.com/watch?v=bV0QNuhN9fU&t=64s
+			let matches = tabTitle.match(/^(\([0-9]+\) )?(.*)$/);
 			if (matches) {
 				tabTitle = matches[2];
 			}
 
-			if (dataFormat == 'md') {
+			// format each url depending on the requested data format
+			if (dataFormat == 'markdown') {
+				//! console.log("markdown acknowledges")
 				let prefix = '';
 				if (isImage(tabUrl)){
 					prefix = '!';
 				}
 				buffer = prefix + '[' + tabTitle + '](' + tabUrl + ')';
 			}
-            else if (dataFormat == 'title') {
-				buffer = tabTitle;
-			}
 			else if (dataFormat == 'url') {
+				//! console.log("url acknowledges")
 				buffer = tabUrl;
 			}
+			else if (dataFormat == 'title') {
+				//! console.log("title acknowledges")
+				buffer = tabTitle;
+			}
 
+			// append working buffer to output, and if there's more than one line left start a new line
 			output += buffer;
-			if (tabs.length > 1){
+			if (tabs.length > 1) {
 				output += '\n';
 			}
+			//! console.log("formatInput: output 1:", output); // too verbose
 		}
+		//! console.log("formatInput: output 2:"); // good
+		//! console.log(output);
+		//! return output;
+		copyToClipboard(output);
+	});
+	//! console.log("formatInput: output 3:", output); // nothing
+}
 
-    });
+function isImage(url) {
+	return (url.indexOf('.jpg') > -1 || url.indexOf('.png') > -1);
+}
+
+// see: https://github.com/zenorocha/clipboard.js/blob/master/demo/target-programmatic-copy.html
+function copyToClipboard(input: string) {
+	const textCopied = ClipboardJS.copy(input);
+	console.log('copied!\n' + textCopied);
+}
+
+// see: https://developer.chrome.com/docs/extensions/reference/tabs/#get-the-current-tab
+async function getCurrentTab() {
+	let queryOptions = { active: true, lastFocusedWindow: true };
+	let [tab] = await chrome.tabs.query(queryOptions);
+	//! 	  ↑ 'tab' will either be a 'tabs.Tab' instance or 'undefined'.
+	return tab;
+}
+
+export function copyTabsHandler(dataFormat, dataAction) {
+	setupOptions(dataFormat, dataAction);
+	//! let object = await setupOptions(dataAction);
+	//! let output = await formatInput(dataFormat, object.options, object.tabStart);
+	//! console.log("formatInput: output:", output);
+	//! copyToClipboard(output);
 }
--- src/copy-tabs/index.ts
+++ src/copy-tabs/index.ts
@@ -1,54 +1,56 @@
-import { getUIDef, UIDef } from '../common/ui';
+import { getUIDef } from '../common/ui';
 import { getStoredOptions, StorageKey, storeValue } from '../common/storage';
 import { debounce } from 'ts-debounce';
+import { copyTabsHandler } from './copy';
 
 export { };
 
 export const SAVE_URL_LIST_DEBOUNCE_TIME_MS = 100;
 export const UPDATE_TAB_COUNT_DEBOUNCE_TIME_MS = 50;
 
-// const saveOptions = async (ui: UIDef, any): Promise<void> => {
-//     if ()
-//        await storeValue<string>(StorageKey.format, ui.copyForm.);
-//     }
-// };
-// const debouncedSaveOptions = debounce(
-//     saveOptions,
-//     SAVE_URL_LIST_DEBOUNCE_TIME_MS
-// );
+/**
+ * reference: https://github.com/kiichi/QuickCopyTitleAndURL
+ * permissions: storage, tabs, clipboardWrite
+ * debug with: chrome.storage.local.get(console.log)
+ */
 
-function addEventListeners(ui: UIDef, query: string) {
-    // see: https://stackoverflow.com/questions/58606047/how-to-use-on-addeventlistener-on-radio-button-in-plain-javascript
-    
-    let matches = ui.copyForm.querySelectorAll(query);
-    for (let i = 0; i < matches.length; i++) {
-        matches[i].addEventListener("change", function() {
-            console.log(this.value);
-            // debouncedSaveOptions(ui, this.value);
-        });
-        console.log(matches[i]);
+const saveOptions = async (name: string, value: string): Promise<void> => {
+    if (name == "format") {
+        await storeValue<string>(StorageKey.tabFormat, value);
+    }   
+    if (name == "scope") {
+        await storeValue<string>(StorageKey.tabScope, value);
     }
-}
+};
+
+const debouncedSaveOptions = debounce(
+    saveOptions,
+    SAVE_URL_LIST_DEBOUNCE_TIME_MS
+);
 
 export const init = async (): Promise<void> => {
     const ui = getUIDef();
-
-    /* restore options */
     const options = await getStoredOptions();
 
-    // document.getElementById("rightwards").checked = true;
-
-    /* add radio button events */
-    addEventListeners(ui, "input[name=\"format\"]");
-    addEventListeners(ui, "input[name=\"scope\"]");
+    /* restore radio button options and add an event listener for each radio button */
+    let matches = ui.copyForm.querySelectorAll("input[type=radio]");
+    for (let i = 0; i < matches.length; i++) {
+        if (options.tabFormat == matches[i].id || options.tabScope == matches[i].id) {
+            // @ts-ignore
+            ui.copyForm.querySelector(`#${matches[i].id}`).checked = true;
+        }
+        matches[i].addEventListener("change", function() {
+            debouncedSaveOptions(this.name, this.id);
+            // debouncedUpdateTabCount(ui);
+        });
+    }
 
     /* add button events */
-    ui.copyButton.addEventListener('click', () => {
-        console.log("click registered");
-        // debouncedSaveOptions(ui);
-        // debouncedUpdateTabCount(ui);
+    // see: https://stackoverflow.com/questions/50623279/js-event-handler-async-function
+    ui.copyButton.addEventListener('click', async () => {
+        const options = await getStoredOptions();
+        copyTabsHandler(options.tabFormat, options.tabScope);
     });
-
 };
 
 document.addEventListener('DOMContentLoaded', init);
