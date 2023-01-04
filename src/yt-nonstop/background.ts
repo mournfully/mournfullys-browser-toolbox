2d7cef22f5998017518c34cb98ed402ad54ce54b
mournfully <minuramolligoda.dev@gmail.com>
2023-01-04 00:40:17 -0500

    working on youtube-nonstop functionality

---
+++ src/yt-nonstop/background.ts
@@ -0,0 +1,23 @@
+/**
+ * see: https://github.com/lawfx/YoutubeNonStop
+ * perms: declarativeContent 
+ */
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { hostEquals: 'www.youtube.com' }
+            }),
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { hostEquals: 'music.youtube.com' }
+            })
+          ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+        }
+      ]);
+    });
+  });
+  
