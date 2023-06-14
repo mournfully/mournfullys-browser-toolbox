c4ca83feb925ea67c9d510ec8f712db481d88f58
mournfully <98147018+mournfully@users.noreply.github.com>
2023-06-14 18:04:53 -0400

    Create a copy of original popup with new stack

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,5 +1,6 @@
 import { render } from 'solid-js/web'
-import app from './components/temp/temp'
+import app from './temp'
+// https://stackoverflow.com/questions/66288645/vite-does-not-build-tailwind-based-on-config
 import './style.css'
 
 const app_container = document.querySelector("#app-container")
