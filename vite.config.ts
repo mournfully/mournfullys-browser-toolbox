0817b983ed20150bdc2f9be3b8614f049b24204b
mournfully <98147018+mournfully@users.noreply.github.com>
2023-08-01 14:46:41 -0400

    Install eslint package

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 // https://crxjs.dev/vite-plugin/getting-started/solid/create-project
+import path from 'path'
+import { glob } from 'glob'
 import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
 import { crx } from '@crxjs/vite-plugin'
+import solidPlugin from 'vite-plugin-solid'
 import manifest from './src/manifest.json'
 
 export default defineConfig({
   plugins: [
     solidPlugin(),
-    crx({ manifest }),
+    crx({ manifest })
   ],
   // https://github.com/crxjs/chrome-extension-tools/issues/648
-  server: { 
+  server: {
     port: 3000,
-    hmr: { port: 3000 }, 
+    hmr: { port: 3000 }
   },
+  // https://stackoverflow.com/a/66877705
+  root: path.join(__dirname, './src'),
+  build: { 
+    outDir: path.join(__dirname, './dist'),
+    emptyOutDir: true
+  }
 })
