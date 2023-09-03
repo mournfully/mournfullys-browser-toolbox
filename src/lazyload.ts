440dc5df51411637a7899f52e9ea7af6eaeab786
Minura Molligoda <github@mournfully.net>
2023-09-03 16:19:57 -0400

    Fix broken `lazyload.ts` with `npm run build`
    Including `lazyload.ts` from 'lazyload.html' would work with `npm run
    dev` but not `npm run build`. It seems both files need to be included
    with `web_accessible_resources` in `manifest.json`

---
+++ src/lazyload.ts
@@ -0,0 +1,17 @@
+document.title =
+  '[' +
+  window.location.hash
+    .substr(1)
+    .replace('http://', '')
+    .replace('https://', '') +
+  ']';
+
+// load site on focus
+window.addEventListener(
+  'focus',
+  () => {
+    window.location.replace(window.location.hash.substr(1));
+  },
+  false
+);
+
